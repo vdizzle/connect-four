725bdbb55fec896b434dafeb000667234b15b670
Vinaya Shrestha <vinayashrestha@gmail.com>
2017-07-26 14:51:32 -0400

    Rename methods

--- lib/connect_four/board.rb
+++ lib/connect_four/board.rb
@@ -7,28 +7,25 @@ module ConnectFour
       @rows = rows
       @columns = columns
       @cells = rows.times.map { |i| Array.new(columns, 0) }
-      @move_count = 0
-      @gameplay_stack_pointer = columns.times.inject({}) do |result, current|
-        result[current] = -1
+      @column_tracker = columns.times.inject({}) do |result, current|
+        result[current] = rows
         result
       end
-      @last_played_cell = []
+      @move_count = 0
+      @active_cell = {}
     end
 
-    def play(column, player_symbol)
+    def play(column, piece)
       target_column = column - 1
-      raise ConnectFour::InvalidMove if gameplay_stack_pointer[target_column].nil?
+      raise ConnectFour::InvalidMove if column_tracker[target_column].nil?
 
-      target_row = gameplay_stack_pointer[target_column] + 1
-      mapped_indices = array_indices_to_board_map(target_row, target_column)
-      mapped_row = mapped_indices.first
-      mapped_column = mapped_indices.last
+      target_row = column_tracker[target_column] - 1
 
-      raise ConnectFour::InvalidMove if mapped_row >= rows || mapped_row < 0
+      raise ConnectFour::InvalidMove if target_row < 0 || target_row >= rows
 
-      cells[mapped_row][mapped_column] = player_symbol
-      @last_played_cell = [target_row, target_column]
-      gameplay_stack_pointer[mapped_column] += 1
+      @active_cell = { row: target_row, column: target_column }
+      set_board_piece_at(active_cell, piece)
+      column_tracker[target_column] -= 1
       @move_count += 1
     end
 
@@ -45,100 +42,130 @@ module ConnectFour
       end
     end
 
-    def has_winner?(player_symbol)
+    def has_winner?(piece)
       return false if move_count < 7
 
-      vertical_connected = vertical_four_connected(player_symbol, *last_played_cell)
+      vertical_connected = vertical_four_connected?(piece, active_cell)
       return true if vertical_connected
 
-      horizontal_connected = horizontal_four_connected(player_symbol, *last_played_cell)
+      horizontal_connected = horizontal_four_connected?(piece, active_cell)
       return true if horizontal_connected
 
-      left_to_right = diagonal_left_to_right_connected(player_symbol, *last_played_cell)
-      return true if left_to_right
+      diagonal_connected = diagonal_positive_slope_four_connected?(piece, active_cell)
+      return true if diagonal_connected
 
-      diagonal_right_to_left_connected(player_symbol, *last_played_cell)
+      diagonal_negative_slope_four_connected?(piece, active_cell)
     end
 
     private
 
-    def array_indices_to_board_map(row, column)
-      [rows - row - 1, column]
+    def set_board_piece_at(cell_position, piece)
+      cells[cell_position[:row]][cell_position[:column]] = piece
     end
 
-    def vertical_four_connected(player_symbol, row, column)
-      return false if row < 0 || row >= rows
-      return false if column < 0 || column >= columns
-
-      connected_count = 0
-      mapped_indices = array_indices_to_board_map(row, column)
-      mapped_row = mapped_indices.first
-      mapped_column = mapped_indices.last
-
-      start_row = 0
-      end_row = rows - 1
-
-      (start_row..end_row).each do |current_row|
-        cell_value = cells[current_row][mapped_column]
-        if cell_value.to_s.downcase == player_symbol.to_s.downcase
-          connected_count += 1
-          return true if connected_count >= 4
-        else
-          connected_count = 0
-        end
-
-        false
-      end
-
-      false
+    def get_board_piece_at(cell_position)
+      cells[cell_position[:row]][cell_position[:column]]
     end
 
-    def horizontal_four_connected(player_symbol, row, column)
-      return false if row < 0 || row >= rows
-      return false if column < 0 || column >= columns
+    def matching_piece?(piece, cell)
+      piece = get_board_piece_at(cell)
+    end
 
-      connected_count = 0
-      mapped_indices = array_indices_to_board_map(row, column)
-      mapped_row = mapped_indices.first
-      mapped_column = mapped_indices.last
-      player_series = []
+    def vertical_four_connected?(piece, cell)
+      start_row = cell[:row] - 3
+      end_row = cell[:row] + 3
 
-      start_column = mapped_column - 3
-      end_column = mapped_column + 3
+      start_row = 0 if start_row < 0
+      end_row = rows - 1 if end_row >= rows
+
+      four_connected?(
+        piece,
+        { row: start_row, column: cell[:column] },
+        { row: end_row, column: cell[:column] },
+        { row: 1, column: 0 }
+      )
+    end
+
+    def horizontal_four_connected?(piece, cell)
+      start_column = cell[:column] - 3
+      end_column = cell[:column] + 3
 
       start_column = 0 if start_column < 0
       end_column = columns - 1 if end_column >= columns
 
-      (start_column..end_column).each do |current_column|
-        cell_value = cells[mapped_row][current_column]
-        if cell_value.to_s.downcase == player_symbol.to_s.downcase
+      four_connected?(
+        piece,
+        { row: cell[:row], column: start_column },
+        { row: cell[:row], column: end_column },
+        { row: 0, column: 1 }
+      )
+    end
+
+    def diagonal_positive_slope_four_connected?(piece, cell)
+      start_row = cell[:row] + 3
+      start_column = cell[:column] - 3
+
+      end_row = cell[:row] - 3
+      end_column = cell[:column] + 3
+
+      start_row = rows - 1 if start_row >= rows
+      end_row = 0 if end_row < 0
+      start_column = 0 if start_column < 0
+      end_column = columns - 1 if end_column >= columns
+
+      four_connected?(
+        piece,
+        { row: start_row, column: start_column },
+        { row: end_row, column: end_column },
+        { row: -1, column: 1 }
+      )
+    end
+
+  def diagonal_negative_slope_four_connected?(piece, cell)
+      start_row = cell[:row] + 3
+      start_column = cell[:column] - 3
+
+      end_row = cell[:row] + 3
+      end_column = cell[:column] + 3
+
+      start_row = 0 if start_row < 0
+      end_row = rows - 1 if end_row >= rows
+      start_column = 0 if start_column < 0
+      end_column = columns - 1 if end_column >= columns
+
+      four_connected?(
+        piece,
+        { row: start_row, column: start_column },
+        { row: end_row, column: end_column },
+        { row: 1, column: 1 }
+      )
+    end
+
+
+    def four_connected?(piece, start_cell, end_cell, step)
+      target_cell = start_cell
+      connected_count = 0
+
+      loop do
+        if matching_piece?(piece, target_cell)
           connected_count += 1
           return true if connected_count >= 4
         else
           connected_count = 0
         end
 
-        false
+        target_cell[:row] += step[:row]
+        target_cell[:column] += step[:column]
+
+        if target_cell == end_cell
+          connected_count += 1 if matching_piece?(piece, target_cell)
+          break
+        end
       end
 
-      return true if player_series.count >= 4
-
-      false
+      connected_count >= 4
     end
 
-    def diagonal_left_to_right_connected(player_symbol, row, column)
-      return false if row < 0 || row >= rows
-      return false if column < 0 || column >= columns
-
-      mapped_indices = array_indices_to_board_map(row, column)
-      puts mapped_indices.inspect
-      puts "row, col - #{row}, #{column}"
-
-	end
-
-    def diagonal_right_to_left_connected(player_symbol, row, column)
-	end
-
-    attr_reader :cells, :move_count, :last_played_cell, :gameplay_stack_pointer
+    attr_reader :cells, :move_count, :active_cell, :column_tracker
   end
 end
